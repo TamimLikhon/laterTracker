874e76505699c79114e4eef39d4742c3bcdee6a4
tamimlikhon456@gmail.com <TamimLikhon>
2025-04-27 02:41:16 +0600

    6

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,5 +1,21 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({ ok: true });
+// app/api/cron/route.ts
+import {NextResponse } from 'next/server'
+		
+export async function GET(
+  req
+) {
+  // If token is valid, proceed with the API logic
+  const authToken = req.headers.get('authorization')?.replace('Bearer ', '');
+    
+  if (authToken !== process.env.CRON_SECRET) {
+    return NextResponse.json(
+      { error: 'Unauthorized access. Invalid token.' },
+      { status: 401 }
+    );
+  }
+		
+  return new NextResponse.json(
+    { message: 'Secure API accessed successfully' }),
+    { status: 200 }
+  
 }
