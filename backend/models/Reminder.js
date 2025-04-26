0397b2f00393e2922ede2ac9f0a80f3ec7915caa
tamimlikhon456@gmail.com <TamimLikhon>
2025-04-26 23:04:01 +0600

    1

--- backend/models/Reminder.js
+++ backend/models/Reminder.js
@@ -1,5 +1,3 @@
-// models/Reminder.ts
-import mongoose, { models, model } from "mongoose";
 import ReminderSchema from "../schema/userReminder";
 
 const Reminder = models.Reminder || model("Reminder", ReminderSchema);
